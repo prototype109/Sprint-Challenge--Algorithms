6ff49796a459e36e6a264ceac227c56d02a04ae1
Brandon Nichols <brandonaronnichols@gamil.com>
2020-06-12 23:32:37 -0500

    finally finished robot_sort.py

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,6 +1,10 @@
 # Understand
 ## I can only use the robots methods, loops and conditionals
 ## can't access class attributes directly
+## can turn light on and off and check to see if light is on
+# Plan
+## I think somem sort of bubble sort is needed, because I have no access to indexes or the array itself
+    ## so I just need to be able to swap until I can't swap anymore
 class SortingRobot:
     def __init__(self, l):
         """
@@ -100,7 +104,23 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        while self.can_move_right():
+            self.swap_item()
+            # self.set_light_on()
+            while self.can_move_right():
+                # print('STUCK')
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    # self.set_light_off()
+
+            while self.can_move_left():
+                # print('STUCK2')
+                self.move_left()
+                if self.compare_item() is None:
+                    self.swap_item()
+                    self.move_right()
+                    break
 
 
 if __name__ == "__main__":
@@ -108,7 +128,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [1, 3 ,2, 4]
     robot = SortingRobot(l)
 
     robot.sort()
